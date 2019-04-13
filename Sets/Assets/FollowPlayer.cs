bcab183638ea3d1130432a607f4873ff39495c5a
Pablo Villalobos <pvillalo@mit.edu>
2019-04-13 11:41:34 -0400

    Added preliminary art for scenes

---
+++ Sets/Assets/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float followAmount = 0.1f;
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPosition + player.position * followAmount;
+    }
+}
