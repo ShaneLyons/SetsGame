f6fc985233caa9b84794a6e94aa294dc69e85c16
Pablo Villalobos <pvillalo@mit.edu>
2019-04-28 21:47:11 -0400

    Add sets things, edit grass

--- Sets/Assets/RustleGrass.cs
+++ Sets/Assets/RustleGrass.cs
@@ -6,9 +6,13 @@ public class RustleGrass : MonoBehaviour
 {
     private Animator anim;
 
+    public List<Sprite> grass;
+
     private void Start()
     {
         anim = transform.GetChild(0).GetComponent<Animator>();
+
+        transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = grass[Random.Range(0, grass.Count)];
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
