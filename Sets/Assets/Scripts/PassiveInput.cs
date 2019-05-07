4677eb4648e8229151db6728b6ea3146944e6ae1
Shane Lyons <slyons365@gmail.com>
2019-05-07 14:35:20 -0400

    Fix dropping sets in front of input bug

--- Sets/Assets/Scripts/PassiveInput.cs
+++ Sets/Assets/Scripts/PassiveInput.cs
@@ -9,10 +9,5 @@ public class PassiveInput : MonoBehaviour, InputBlock {
     public void InputSet(HashSet<Jewel> inputSet) {
         set = inputSet;
         GetComponentInParent<Operator>().InputSet(set, isLeftInput);
-        // take this out later, I added this for class playtest
-        if (inputSet.Count == 2)
-        {
-            GetComponentInChildren<SetController>().showSet();
-        }
     }
 }
