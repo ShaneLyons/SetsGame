69ba030a144cc81b8a6a26c3ef6270c2ad8252d0
Shane Lyons <slyons365@gmail.com>
2019-05-07 15:11:06 -0400

    Have fixed and passive inputs automatically populate

--- Sets/Assets/Scripts/FixedInput.cs
+++ Sets/Assets/Scripts/FixedInput.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class FixedInput : MonoBehaviour, InputBlock {
     public bool isLeftInput;
+    public GameObject jewelPrefab;
 
+    // used for setting up the set
     [SerializeField]
-    private static int setSize = 3;
+    private static int setSize = 2;
     public Jewel[] jewels = new Jewel[setSize];
 
     void Start() {
         HashSet<Jewel> set = new HashSet<Jewel>(jewels);
         InputSet(set);
+        populateView(set);
     }
 
     public void InputSet(HashSet<Jewel> inputSet) {
         GetComponentInParent<Operator>().InputSet(inputSet, isLeftInput);
     }
 
-    private void populateView()
+    private void populateView(HashSet<Jewel> inputSet)
     {
-
+        // TODO: will probably have to change these to accompany multiple amounts of sets0
+        float offset = .2f;
+        float xOffset = .1f;
+        float yOffset = .15f;
+        float scaleFactor = .5f;
+        for (int i=0; i < setSize; i++)
+        {
+            GameObject jewelSprite = Instantiate(jewelPrefab, transform);
+            jewelSprite.GetComponent<JewelController>().setColor(jewels[i]);
+            Vector3 scale = jewelSprite.transform.localScale;
+            Vector3 pos = jewelSprite.transform.position;
+            jewelSprite.transform.localScale = new Vector3(scale.x * .5f, scale.y, scale.z) * scaleFactor;
+            jewelSprite.transform.position = new Vector3(pos.x - xOffset + offset * i, pos.y + yOffset, pos.z);
+        }
     }
 }
--- Sets/Assets/Scripts/PassiveInput.cs
+++ Sets/Assets/Scripts/PassiveInput.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PassiveInput : MonoBehaviour, InputBlock {
+    public GameObject jewelPrefab;
+
     public bool isLeftInput;
     private HashSet<Jewel> set;
 
     public void InputSet(HashSet<Jewel> inputSet) {
         set = inputSet;
         GetComponentInParent<Operator>().InputSet(set, isLeftInput);
+        populateView(inputSet);
+    }
+
+    private void populateView(HashSet<Jewel> inputSet)
+    {
+        Jewel[] jewels = new Jewel[inputSet.Count];
+        inputSet.CopyTo(jewels);
+        // TODO: will probably have to change these to accompany multiple amounts of sets
+        float offset = .2f;
+        float xOffset = .1f;
+        float yOffset = .15f;
+        float scaleFactor = .5f;
+        for (int i = 0; i < inputSet.Count; i++)
+        {
+            GameObject jewelSprite = Instantiate(jewelPrefab, transform);
+            jewelSprite.GetComponent<JewelController>().setColor(jewels[i]);
+            Vector3 scale = jewelSprite.transform.localScale;
+            Vector3 pos = jewelSprite.transform.position;
+            jewelSprite.transform.localScale = new Vector3(scale.x * .5f, scale.y, scale.z) * scaleFactor;
+            jewelSprite.transform.position = new Vector3(pos.x - xOffset + offset * i, pos.y + yOffset, pos.z);
+        }
     }
 }
