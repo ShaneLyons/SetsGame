dc47472a89f9064ee7b88c4ad9e1b81716097e4d
Jocelyn McGhee <jmcghee@mit.edu>
2019-05-13 13:17:41 -0400

    Fixed bug with teleporters

--- Sets/Assets/Scripts/Teleporter.cs
+++ Sets/Assets/Scripts/Teleporter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleporter : MonoBehaviour, Goal
 {
@@ -8,14 +9,27 @@ public class Teleporter : MonoBehaviour, Goal
     public Sprite teleporter_lit;
     public GameObject lightBeam;
     public SpriteRenderer spriteRenderer;
+    public string sceneName;
+    public GameObject player;
+    private bool on;
     // Start is called before the first frame update
     void Start()
     {
         lightBeam.gameObject.SetActive(false);
+        on = false;
         spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
         if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
         {
             spriteRenderer.sprite = teleporter_unlit; // set the sprite to teleporter_unlit
+        }
+    }
+
+    void Update()
+    {
+        
+        if (transform.position.x < player.transform.position.x && on)
+        {
+            loadByIndex();
         }
     }
 
@@ -24,6 +38,11 @@ public class Teleporter : MonoBehaviour, Goal
         spriteRenderer.sprite = teleporter_lit;
         lightBeam.gameObject.SetActive(true);
         //FindObjectOfType<AudioManagerController>().Play("DoorMoving");
+    }
+
+    public void loadByIndex()
+    {
+        SceneManager.LoadScene(sceneName);
     }
 
     public void FailureState()
@@ -38,10 +57,12 @@ public class Teleporter : MonoBehaviour, Goal
     {
         if (isCorrect)
         {
+            on = true;
             SuccessState();
         }
         else
         {
+            on = false;
             FailureState();
         }
     }
