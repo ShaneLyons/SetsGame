cd4e36083f35c692c342b1142951a23acab1172c
Jocelyn McGhee <jmcghee@mit.edu>
2019-05-13 08:56:37 -0400

    Added an in game menu to reach level select

---
+++ Sets/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameMenu : MonoBehaviour
+{
+    public Button menuButton;
+    public GameObject panel;
+    public Button screen;
+    public bool active;
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+        menuButton.onClick.AddListener(ToggleMenu);
+        screen.onClick.AddListener(HideMenu);
+    }
+
+    void HideMenu()
+    {
+        active = false;
+        panel.gameObject.SetActive(false);
+        
+    } 
+
+    void ToggleMenu()
+    {
+        active = !active;
+        if (active)
+        {
+            panel.gameObject.SetActive(true);
+        }
+        else
+        {
+            panel.gameObject.SetActive(false);
+        }
+    }
+}
