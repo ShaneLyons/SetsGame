0c9fa64bc526d6f7ac8ee7f6fef66844d7532ed8
Jini Gabbidon <jini@mit.edu>
2019-05-14 22:26:32 -0400

    Added button audio.

---
+++ Sets/Assets/Scripts/ButtonSounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonSounds : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+{
+    public void OnPointerEnter(PointerEventData ped)
+    {
+        FindObjectOfType<AudioManagerController>().Play("MouseOverButton");
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        FindObjectOfType<AudioManagerController>().Play("MouseClickButton");
+    }
+}
